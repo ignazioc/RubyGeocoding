376384d367521c9facedc680e3993690fe4372f0
Ignazio Calò <i.calo@reply.it>
2014-01-09 15:46:40 +0100

    Aggiunto numero massimo di request.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Simple geocoder.
+Simple geocoder written in ruby.
+This script process  max 2500 row for every run, to respect Google api limitations.
+
 
 Thanks to: http://www.scholarslab.org/digital-humanities/geocoding-for-neatline-part-i/
--- script.rb
+++ script.rb
@@ -1,35 +1,64 @@
 require 'csv'
 require 'geocoder'
- 
-LOCATIONS = './locations.csv'
- 
-def degrees_to_meters(lon, lat)
-    half_circumference = 20037508.34
-    x = lon * half_circumference / 180
-    y = Math.log(Math.tan((90 + lat) * Math::PI / 360)) / (Math::PI / 180)
- 
-    y = y * half_circumference / 180
- 
-    return [x, y]
-end
- 
+require 'english'
+require 'yaml'
+
+ARCHIVE_FILENAME = 'db.csv'
+RESP_FILENAME = 'output.csv'
+
+LOCATIONS = File.dirname(__FILE__) + "/" + ARCHIVE_FILENAME
+COORDINATE = File.dirname(__FILE__) + "/" + RESP_FILENAME 
+
+
+puts "File archivio: #{LOCATIONS}"
+puts "File risposte: #{COORDINATE}"
+
+
+#Verifico il numero di righe già scritte sul file di output
+#e quelle le salto
+num_row = %x{wc -l '#{COORDINATE}'}.to_i 
+puts "Numero di righe già processate: #{num_row}"
+row_to_skip = num_row
+
+
+MAX_REQUEST = 2500;
+TIMEOUT = 1;
+
 Geocoder.configure({
-	lookup: :google,
-	http_proxy: "http://proxy.reply.it:8080" 
+	lookup: :google
+	#http_proxy: "http://proxy.xxxxxxx.it:8080"
 })
 
-# puts 'address,city,state,point,lat,lon'
-CSV.foreach(LOCATIONS, :headers => true, :header_converters => :symbol) do |line|
-    address_string = "#{line[:address]}, #{line[:city]}, #{line[:state]}"
-    result = Geocoder.search(address_string).first
- 
-    lat = result.latitude
-    lon = result.longitude
- 
-    point = "POINT(#{lon} #{lat})"
-    projected = degrees_to_meters(lon, lat)
-    point = "POINT(#{projected[0]} #{projected[1]})"
- 
-    puts "#{address_string}, #{point}, #{lat}, #{lon}"
-    #puts address_string
-end
+request_count = 0
+File.open(COORDINATE, 'a') { |f|
+	
+	
+	CSV.foreach(LOCATIONS, encoding: "utf-8", :headers => true, :header_converters => :symbol, :col_sep => ";") do |line|
+		if (request_count >= MAX_REQUEST)
+			puts "Raggiunto limite massimo di quest (#{request_count})"
+			break
+		end
+		
+		#Se ho processato x righe, ne devo saltare x+1 perché nel file archivio bisogna contare l'header
+		next if $INPUT_LINE_NUMBER <= (row_to_skip + 1)
+		
+		request_count = request_count + 1
+		
+		address_string = "#{line[:street]}, #{line[:postalcode]}, #{line[:city]}, #{line[:region]}, #{line[:country]}"
+		
+		puts "Riga n. #{$INPUT_LINE_NUMBER - 1}, #{address_string}"
+		
+		result = Geocoder.search(address_string).first
+		if (result)
+			lat = result.latitude
+			lon = result.longitude
+			f.puts "Riga n. #{$INPUT_LINE_NUMBER - 1}, #{line[:partner]}, #{line[:address]}, #{address_string},  #{lat}, #{lon}"
+			puts "Riga n. #{$INPUT_LINE_NUMBER - 1}, #{line[:partner]}, #{line[:address]}, #{address_string},  #{lat}, #{lon}"
+		else
+			f.puts "Riga n. #{$INPUT_LINE_NUMBER - 1}, #{line[:partner]}, #{line[:address]}, #{address_string}, nan, nan"
+			puts "Riga n. #{$INPUT_LINE_NUMBER - 1}, #{line[:partner]}, #{line[:address]}, #{address_string}, nan, nan"
+		end
+		sleep(TIMEOUT)
+	end
+
+}
